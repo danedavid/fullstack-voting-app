63d7ab7b396909354515f6a51fb5878996a6ceaa
danedavid <dndavid102@gmail.com>
2018-04-16 19:11:54 +0530

    Basic client set up (using react-redux-boilerplate)

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactLogo from '../assets/images/react-logo.png';
+import ReduxLogo from '../assets/images/redux-logo.png';
+import './App.scss';
+
+const App = () => (
+  <div className='app-container'>
+    Voting App
+  </div>
+);
+
+export default App;

b53b233f43bf7c4845ecb574085d768587c48379
dane <dane@qburst.com>
2018-04-17 15:35:18 +0530

    Client app styles and event listeners

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,26 +1,64 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 import './App.scss';
 
-const App = ({ data }) => (
-  <Fragment>
-    <div className='thumbnail-container'>
-      {
-        data.techs &&
-          data.techs.map(item => (
-            <div key={item.id} className='tech-item-card'>
-              <div
-                className='tech-item-logo'
-                style={{ backgroundImage: `url(${item.image})` }}
-              />
-              {item.name}
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedId: 'react'
+    };
+  }
+
+  handleClick(id) {
+    const { selectedId } = this.state;
+    if ( selectedId === id ) {
+      this.setState({ selectedId: null });
+      return;
+    }
+    this.setState({ selectedId: id });
+  }
+
+  render() {
+    const { data } = this.props;
+    const { selectedId } = this.state;
+
+    return (
+      <Fragment>
+        <div className='header'>Voting App</div>
+        <div className='thumbnail-container'>
+          {
+            !data.techs &&
+              <div>Loading...</div>
+          }
+          {
+            data.techs &&
+              data.techs.map(({ id, name, image }) => (
+                <div
+                  key={id}
+                  className={'tech-item-card' + (selectedId === id ? ' selected-card': '')}
+                  onClick={this.handleClick.bind(this, id)}
+                >
+                  <div
+                    className='tech-item-logo'
+                    style={{ backgroundImage: `url(${image})` }}
+                  />
+                  <span className='tech-item-name'>{name}</span>
+                </div>
+              ))
+          }
+        </div>
+        {
+          selectedId &&
+            <div className='vote-button-container'>
+              <button className='vote-button'>Vote!</button>
             </div>
-          ))
-      }
-    </div>
-  </Fragment>
-);
+        }
+      </Fragment>
+    );
+  }
+}
 
 export default graphql(
   gql`

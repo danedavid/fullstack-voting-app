a523971a003563db659a9cedc7ad4bd50a232b7e
dane <dane@qburst.com>
2018-04-26 18:18:04 +0530

    Implement own GH login [WIP]

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
-import {
-  Link
-} from 'react-router-dom';
+import GithubLogin from 'react-github-login';
 //import './Login.scss';
 
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      code: null
+    };
+    this.handleSuccess = this.handleSuccess.bind(this);
+  }
+
+  handleSuccess(obj) {
+    this.props.history.push('/dashboard', obj);
+  }
 
   handleClick() {
-    this.props.history.push('/dashboard', { clientId: 'hakunamatata' });
+    let win = window.open('https://github.com/login/oauth/authorize?client_id=1759a4054580059ed00e','name', 'width=500,height=600');
+    let code = null;
+    setInterval(() => {
+      if ( win.location.search ) {
+        code =  win.location.search.match(/\?code=([a-zA-Z0-9]*)/)[1];
+        this.setState({ code });
+        win.close();
+      }
+    }, 1000);
+    return;
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.handleClick.bind(this)}>click me to login</button>
+        {
+          this.state.code && <div>{this.state.code}</div>
+        }
+        <GithubLogin
+          clientId='1759a4054580059ed00e'
+          redirectUri='http://localhost:8000'
+          onSuccess={this.handleSuccess}
+          onFailure={(err) => console.log('Error Logging in: ', err)}
+        />
+        <button onClick={this.handleClick.bind(this)}>login</button>
       </div>
     );
   }

75ef5f101a3f201fbe6571999001e4e66553241c
danedavid <dndavid102@gmail.com>
2018-05-10 22:42:17 +0530

    Handle Github API using server

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -6,19 +6,18 @@ class Login extends Component {
   constructor() {
     super();
     this.state = {
-      code: null
+      accessToken: null
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
     let win = window.open('https://github.com/login/oauth/authorize?client_id=1759a4054580059ed00e','name', 'width=500,height=600');
-    let code = null;
+    let accessToken = null;
     let intervalID = setInterval(() => {
-      console.log('setInterval')
       if ( win.location.search ) {
-        code =  win.location.search.match(/\?code=([a-zA-Z0-9]*)/)[1];
-        this.setState({ code });
+        accessToken =  win.location.search.match(/\?access_token=([a-zA-Z0-9]*)/)[1];
+        this.setState({ accessToken });
         clearInterval(intervalID);
         win.close();
       }
@@ -32,35 +31,19 @@ class Login extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    const { code } = this.state;
-    if ( prevState.code === null && code ) {
-      window.fetch('https://github.com/login/oauth/access_token', {
-        method: 'POST',
-        body: {
-          client_id: '1759a4054580059ed00e',
-          client_secret: 'b02f2a6956ec13d1bf9b5b82be60116c667dfde0',
-          code
-        },
-        headers: {
-          'Accept': 'application/json'
-        }
-      })
-        .then(res => {
-          console.log(res);
-          return res;
-        })
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(err => console.log(err));
+    const { accessToken } = this.state;
+    if ( prevState.accessToken === null && accessToken ) {
+      this.props.history.push('/dashboard');
     }
   }
 
   render() {
+    const { accessToken } = this.state;
+    if ( accessToken ) {
+      return <div>Redirecting...</div>;
+    }
     return (
       <div>
-        {
-          this.state.code && <div>{this.state.code}</div>
-        }
         <button onClick={this.handleClick}>Login with Github</button>
       </div>
     );
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const compression = require('compression');
+const request = require('request');
 const { graphql, graphiql } = require('./server/server');
 
 const app = express();
@@ -15,11 +16,29 @@ app.use(express.static(path.resolve(__dirname, 'client', 'dist')));
 app.post('/graphql',  bodyParser.json(), graphql);
 app.get('/graphiql',  bodyParser.json(), graphiql);
 
+app.get('/authorize', (req,res) => {
+  const { code } = req.query;
+
+  request({
+    //TODO: take client_secret from DB
+    url: `https://github.com/login/oauth/access_token?code=${code}&client_id=1759a4054580059ed00e&client_secret=b02f2a6956ec13d1bf9b5b82be60116c667dfde0`,
+    method: 'POST',
+    json: true
+  }, (err, response) => {
+    if ( err ) {
+      console.log('Error with GH API: ', err);
+    } else {
+      const { access_token } = response.body;
+
+      res.redirect(`/?access_token=${access_token}`);
+    }
+  })
+});
+
 // Serve client app
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
 });
-
 
 app.listen(PORT, () => {
   console.log(`App listening on ${PORT}`)
